beecdc4ba359ffb9db3dd52a4e1b909b45c416c9
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2025-11-30 19:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+function insertRow() {
+    let table = document.getElementById("myTable");
+
+    // Insert row at top (index 0)
+    let newRow = table.insertRow(0);
+
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 }
